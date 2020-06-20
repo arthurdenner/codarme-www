1df21286cc2044a62662d66b3ff3785325bd438f
Bruno Bertolini <dev@brunobertolini.com>
2020-06-20 14:18:40 -0300

    chore: add env indicator when not in production

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,17 @@ import '~/styles/index.css'
 import { Analytics } from '~/components/Analytics'
 import Head from 'next/head'
 
+const isProd =
+  !process.env.NEXT_PUBLIC_DEPLOYMENT_ENV ||
+  process.env.NEXT_PUBLIC_DEPLOYMENT_ENV === 'production'
+
+const Env = () => (
+  <div className="bg-red rounded-md p-2 absolute m-4 text-white bottom-0">
+    <small>ENV:</small>{' '}
+    <strong>{process.env.NEXT_PUBLIC_DEPLOYMENT_ENV}</strong>
+  </div>
+)
+
 function App({ Component, pageProps }) {
   return (
     <>
@@ -9,6 +20,7 @@ function App({ Component, pageProps }) {
       <Head>
         <title>Codar.me - Desenvolvendo software de verdade</title>
       </Head>
+      {!isProd && <Env />}
       <Component {...pageProps} />
     </>
   )
