e2ee04576b6716f90120841739eda35f41bed1c4
Bruno Bertolini <dev@brunobertolini.com>
2020-10-12 09:49:48 -0300

    fix: scroll

--- pages/links/index.js
+++ pages/links/index.js
@@ -21,20 +21,22 @@ const Card = ({ title, destination, slug }) => {
 
 const Links = () => (
   <div
-    className="bg-cover flex flex-1 flex-col h-full p-4 h-screen"
+    className="bg-cover flex flex-1 flex-col h-screen py-4"
     style={{ backgroundImage: 'url(/img/bg-hero.png)' }}
   >
-    <div className="container max-w-xl">
-      <div className="flex flex-col items-center">
-        <Logo color="#fff" width={220} />
-        <div class="mt-6 mb-6">
-          <SocialLinks />
+    <div className="flex overflow-scroll px-4">
+      <div className="container max-w-xl">
+        <div className="flex flex-col items-center">
+          <Logo color="#fff" width={220} />
+          <div className="mt-6 mb-6">
+            <SocialLinks />
+          </div>
         </div>
-      </div>
-      <div>
-        {cards.map((card) => (
-          <Card key={card.slug} {...card} />
-        ))}
+        <div>
+          {cards.map((card) => (
+            <Card key={card.slug} {...card} />
+          ))}
+        </div>
       </div>
     </div>
   </div>
