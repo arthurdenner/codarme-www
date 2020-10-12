70931a2d3b506eedb826167bd30d776c8dc7e09a
Bruno Bertolini <dev@brunobertolini.com>
2020-10-12 09:34:21 -0300

    fix: links destinations and screen full

--- pages/links/index.js
+++ pages/links/index.js
@@ -1,16 +1,17 @@
 import { Logo } from '~/components/Logo'
 import { SocialLinks } from '~/components/SocialLinks'
-import { cards } from '~/utils/cards'
+
+import cards from './links.json'
 
 const Card = ({ title, destination, slug }) => {
   const handleClick = () => {
-    fbq('track', destination, { title, destination, slug })
+    fbq('track', 'linkClick', { title, destination, slug })
     window.open(destination)
   }
 
   return (
     <div
-      className="border-2 text-white mt-6 rounded-md p-4 text-center bg-gunmetal border-gray cursor-pointer hover:bg-green hover:font-bold transition duration-500 ease-in-out"
+      className="border-2 text-white mt-6 rounded-md p-4 text-center bg-gunmetal border-gray cursor-pointer hover:border-green hover:bg-green hover:font-bold transition duration-500 ease-in-out"
       onClick={handleClick}
     >
       <p>{title}</p>
@@ -20,10 +21,10 @@ const Card = ({ title, destination, slug }) => {
 
 const Links = () => (
   <div
-    className="bg-cover flex flex-1 flex-col h-full p-4"
+    className="bg-cover flex flex-1 flex-col h-full p-4 h-screen"
     style={{ backgroundImage: 'url(/img/bg-hero.png)' }}
   >
-    <div className="container">
+    <div className="container max-w-xl">
       <div className="flex flex-col items-center">
         <Logo color="#fff" width={220} />
         <div class="mt-6 mb-6">
@@ -31,7 +32,7 @@ const Links = () => (
         </div>
       </div>
       <div>
-        {cards.map(card => (
+        {cards.map((card) => (
           <Card key={card.slug} {...card} />
         ))}
       </div>
