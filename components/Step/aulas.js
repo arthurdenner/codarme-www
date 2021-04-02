fc09ac430b84a17d29ea5790f491696753648f9e
diego <diegooliveira.tsi@gmail.com>
2021-04-02 12:27:44 -0300

    feat: crate logical compoennt to handle aulas

---
+++ components/Step/aulas.js
@@ -0,0 +1,9 @@
+export const aulas = [
+  {name: 'Aula 01', day: '05/04', slug: 'aula-01', available: process.env.NEXT_PUBLIC_AULA_01, current: false },
+  {name: 'Aula 02', day: '06/04',  slug: 'aula-02', available: process.env.NEXT_PUBLIC_AULA_02, current: false },
+  {name: 'Aula 03', day: '07/04', slug: 'aula-03', available: process.env.NEXT_PUBLIC_AULA_03, current: false },
+  {name: 'Aula 04', day: '08/04', slug: 'aula-04', available: process.env.NEXT_PUBLIC_AULA_04, current: false },
+  {name: 'Aula 05', day: '09/04', slug: 'aula-05', available: process.env.NEXT_PUBLIC_AULA_05, current: false }
+]
+
+
--- components/Step/index.js
+++ components/Step/index.js
@@ -1,9 +1,41 @@
-export const Step = ({date, text, disable = false}) => (
-  <div className={`flex cursor-${disable ? 'not-allowed' : 'pointer'} items-center mb-4`}>
-    <div className={`text-${disable ? 'disable' : 'white'} text-center`}>
-        <p>{date}</p>
-        <p className="font-bold">{text}</p>
-    </div>
-    <div className={`line ${!disable ? 'active' : ''}`} disable={disable} />
-  </div>
-)
+import {useRouter} from 'next/router'
+import { aulas } from './aulas'
+
+
+
+const Badge = ({isAvailable, index}) => 
+  aulas[index].available  ? <div className={`bg-${isAvailable[index] ? 'green' : 'gunmetal'} rounded-md pr-2 pl-2 pt-1 pb-1 text-${isAvailable[index] ? 'white' : 'green'} uppercase mt-2`}>
+  Liberada
+</div> : <></>
+
+
+const isLatest = (index) => index < aulas.length - 1
+
+const lineClass = (index, currentSlug, isAvailable) => 
+  aulas[index + 1].available && isAvailable[index] && aulas[index].available !== currentSlug ? 'line active' : 'line'
+
+const contentClass = (available) => `text-${!available ? 'disable' : 'white '} cursor-${!available ? 'not-allowed' : 'pointer'} text-center `
+
+
+export const Step = ({ isAvailable }) => {
+
+  const route = useRouter()
+  const currentSlug = route.pathname.split('/')[2]
+  const redirectTo = (to) => route.push(`/semana-full-stack-sem-custo/${to}`)
+
+  
+  return (
+    aulas.map(({available, name, day}, index) => (
+      <div className="flex items-center mb-4" key={day} >
+        <div className={contentClass(available)} onClick={() => available && redirectTo(available)}>
+            <p>{day}</p>
+            <p className="font-bold text-xl" >{name}</p>
+            <Badge isAvailable={isAvailable} index={index} />
+        </div>
+        {isLatest(index) && (
+           <div className={lineClass(index, currentSlug, isAvailable)} disable={!available} />
+        )}
+      </div>
+    ))
+  
+)}
