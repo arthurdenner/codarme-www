4b42144f44b101d4d25519f2e2a1248000d4c93a
diego <diegooliveira.tsi@gmail.com>
2021-04-04 17:50:41 -0300

    fix: adjust button link

--- components/Vendas/components/Button/index.js
+++ components/Vendas/components/Button/index.js
@@ -1,5 +1,6 @@
 export const Button = (props) => (
-  <div className="p-4 border-2 w-full" style={{ borderImage:  'url(/img/border-button.png) 5 stretch'}} href="https://pay.hotmart.com/A49443038S" target="_blank">
+  <a href="https://pay.hotmart.com/A49443038S" target="_blank">
+  <div className="p-4 border-2 w-full" style={{ borderImage:  'url(/img/border-button.png) 5 stretch'}}>
     <div className="rounded-full p-2 border-2 border-green w-full">
       <button
         className="p-6 rounded-full bg-green font-bold w-full"
@@ -8,5 +9,6 @@ export const Button = (props) => (
         </button>
     </div>
   </div>
+  </a>
 
 );
