fbec87c93a2f34a28ddfc34e53352325b64e7059
diego <diegooliveira.tsi@gmail.com>
2021-04-06 13:08:19 -0300

    fix: change hotmart link

--- components/Vendas/components/Button/index.js
+++ components/Vendas/components/Button/index.js
@@ -1,5 +1,5 @@
 export const Button = (props) => (
-  <a href="https://pay.hotmart.com/A49443038S" target="_blank">
+  <a href="https://pay.hotmart.com/W24859137T" target="_blank">
   <div className="p-4 border-2 w-full" style={{ borderImage:  'url(/img/border-button.png) 5 stretch'}}>
     <div className="rounded-full p-2 border-2 border-green w-full">
       <button
