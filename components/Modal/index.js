e97b778ffca21a38e56bbf44d8ca32abc6ded105
Bruno Bertolini <dev@brunobertolini.com>
2021-04-11 20:33:36 -0300

    chore: remove webinar page and content

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -7,52 +7,52 @@ import { Button } from '~/components/Button'
 export const Modal = () => {
   const [visible, setVisible] = useState(true)
   return (
-    visible ? 
-    <>
-    <div
-      className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none p-4 md:p-0"
-    >
-    <div className="relative w-auto my-6 mx-auto max-w-2xl">
-    
-      <div className="rounded-lg shadow-lg relative flex flex-col w-full bg-gunmetal outline-none focus:outline-none">
-     
-        <div className="flex items-center justify-center p-5">
-          <img src="/img/curso-logo.png" />
-          <button
-            className="p-1 absolute right-0 top-0 bg-transparent border-0 text-black text-8xl font-semibold outline-none focus:outline-none"
-            onClick={() => setVisible(false)}
-          >
-            <span className="bg-transparent text-white opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-              ×
-            </span>
-          </button>
-        </div>
-    
-        <div className="relative px-4 md:px-24 text-center text-white flex-auto flex flex-col items-center justify-center">
-          <p className="my-4 text-2xl">
-          Torne-se um profissional acima da média e requisitado pelo mercado.
-          </p>
-          <p className="my-4  text-lg text-lighterGray">
-          Aprenda a desenvolver um software de verdade, de qualidade, com quem tem muita experiência prática.
-          </p>
-          <div className="md:w-2/4 w-full mt-8">
-          <a href="/inscricoes">
-            <Button>
-              <p className="text-white font-bold">
-              Quero saber mais!
-              </p>
-            </Button>
-          </a>
+    visible ?
+      <>
+        <div
+          className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none p-4 md:p-0"
+        >
+          <div className="relative w-auto my-6 mx-auto max-w-2xl">
+
+            <div className="rounded-lg shadow-lg relative flex flex-col w-full bg-gunmetal outline-none focus:outline-none">
+
+              <div className="flex items-center justify-center p-5">
+                <img src="/img/curso-logo.png" />
+                <button
+                  className="p-1 absolute right-0 top-0 bg-transparent border-0 text-black text-8xl font-semibold outline-none focus:outline-none"
+                  onClick={() => setVisible(false)}
+                >
+                  <span className="bg-transparent text-white opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    ×
+                  </span>
+                </button>
+              </div>
+
+              <div className="relative px-4 md:px-24 text-center text-white flex-auto flex flex-col items-center justify-center">
+                <p className="my-4 text-2xl">
+                  Torne-se um profissional acima da média e requisitado pelo mercado.
+                </p>
+                <p className="my-4  text-lg text-lighterGray">
+                  Aprenda a desenvolver um software de verdade, de qualidade, com quem tem muita experiência prática.
+                </p>
+                <div className="md:w-2/4 w-full mt-8">
+                  <a href="/inscricoes">
+                    <Button>
+                      <p className="text-white font-bold">
+                        Quero saber mais!
+                      </p>
+                    </Button>
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-center justify-end p-4 rounded-b">
+
+              </div>
+            </div>
           </div>
         </div>
-        <div className="flex items-center justify-end p-4 rounded-b">
-         <Logo color="white" width={120}/>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className="opacity-75 fixed inset-0 z-40 bg-black" />
-  </>
-  : <></>
+        <div className="opacity-75 fixed inset-0 z-40 bg-black" />
+      </>
+      : <></>
   )
 }
--- pages/webinario/index.js
+++ pages/webinario/index.js
@@ -1,42 +1,21 @@
-import { Hero } from '~/components/Hero'
-
-const Title = () => {
-  return (
-    <h1 className="p-1 text-2xl md:text-4xl lg:text-5xl mb-6">
-      Quer <span className="text-green">evoluir</span> como dev ou  <span className="text-green">desenvolver</span> aquela sua aplicação que não sai do papel?
-    </h1>
-
-  )
-}
-
-const Presentation = () => (
-  <div className="p-2 text-white md:flex-1">
-    <p className="mt-4 font-bold" style={{ fontSize: 18 }}>
-      Então vem com a gente nesse webinário.
-    </p>
-    <p className="mt-2 xl:text-1xl">
-      Vamos te mostrar como criar uma aplicação completa, front, back e banco de dados, e o melhor: sem gastar nada!
-    </p>
-    <p className="mt-2 xl:text-1xl">
-      Se inscreve pra esse webinário incrível que vai abrir sua mente, e fica ligado no seu e-mail, pois vem muita coisa boa por ai e vamos avisar das datas e horários por lá.
-    </p>
-    <p className="mt-6 xl:text-1xl font-bold">
-      Ao infinito e além!!!
-    </p>
-
-  </div>
+const Week = () => (
+  <></>
 )
 
+Week.getInitialProps = ({ res }) => {
+  const location = '/'
 
-const Webinario = () => {
-  return (
-    <div
-      className="bg-cover flex flex-1 flex-col h-full"
-      style={{ backgroundImage: 'url(/img/bg-hero.png)' }}
-    >
-      <Hero presentation={Presentation} title={Title} sendPulseType="webinario" />
-    </div>
-  )
+  if (res) {
+    res.writeHead(302, {
+      Location: location
+    })
+
+    res.end()
+  }
+
+  Router.push(location)
 }
 
-export default Webinario
+
+export default Week
+
