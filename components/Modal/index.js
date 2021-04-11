d5625c991f4bfeaf2da613c998380f7181d8e6c8
diegotsi <diegooliveira.tsi@gmail.com>
2021-04-11 19:31:37 -0300

    fix: adjust modal

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -12,11 +12,11 @@ export const Modal = () => {
     <div
       className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none p-4 md:p-0"
     >
-    <div className="relative w-auto my-6 mx-auto max-w-2xl bg-gunmetal">
+    <div className="relative w-auto my-6 mx-auto max-w-2xl">
     
-      <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gunmetal outline-none focus:outline-none">
+      <div className="rounded-lg shadow-lg relative flex flex-col w-full bg-gunmetal outline-none focus:outline-none">
      
-        <div className="flex items-center justify-center p-5 rounded-t">
+        <div className="flex items-center justify-center p-5">
           <img src="/img/curso-logo.png" />
           <button
             className="p-1 absolute right-0 top-0 bg-transparent border-0 text-black text-8xl font-semibold outline-none focus:outline-none"
@@ -39,7 +39,7 @@ export const Modal = () => {
           <a href="/inscricoes">
             <Button>
               <p className="text-white font-bold">
-                Garantir minha vaga
+              Quero saber mais!
               </p>
             </Button>
           </a>
@@ -51,7 +51,7 @@ export const Modal = () => {
       </div>
     </div>
   </div>
-  <div className="opacity-25 fixed inset-0 z-40 bg-black" />
+  <div className="opacity-75 fixed inset-0 z-40 bg-black" />
   </>
   : <></>
   )
